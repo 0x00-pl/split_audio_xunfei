6a8a83b244f679702a3c3f9c0130ef1b738cd8e0
0x00-pl <0x00.pl@gmail.com>
2018-10-04 09:01:24 +0100

    add code

--- mian.py
+++ mian.py
@@ -1,24 +1,31 @@
 import json
 
 
-def load_slices(log_json):
-    return []
+def load_slices(log_json, dst_path):
+
+
+    return {
+        'beg': slice_beg,
+        'end': slice_end,
+        'dst_name': dst_name
+    }
 
 
 def load_audio(file_name):
     return []
 
 
-def export_audio(audio, file_name):
-    pass
+def export_audio(audio, file_name, format='wav'):
+    audio.export(file_name, format=format)
 
 
 def slice_audio(audio, slices, cb):
     for slice_item in slices:
-        cb(audio, slice_item["dst_name"])
+        audio_slice = audio[slice_item["beg"] : slice_item["end"]]
+        cb(audio_slice, slice_item["dst_name"])
 
 
-def main(json_file, src_audio):
-    slices = load_slices(json.load(open(json_file)))
+def main(json_file, src_audio, dst_path):
+    slices = load_slices(json.load(open(json_file)), dst_path)
     audio = load_audio(src_audio)
     slice_audio(audio, slices, export_audio)
